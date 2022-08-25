4b387747d7608197d6a506dab28124fa9ac3c540
Philip Carlsson <philipcarlsson@hotmail.com>
2022-08-25 11:24:51 +0200

    adding pages for squad and players

---
+++ components/players/PlayerDetail.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PlayerDetail(props) {
+  return (
+    <section>
+      <p> in here info about the player will be shown, img etc.</p>
+    </section>
+  );
+}
+
+export default PlayerDetail;
---
+++ pages/truppen/[playerId]/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PlayerDetail from "../../../components/players/PlayerDetail";
+
+function PlayerPage() {
+  return (
+    <div>
+      <PlayerDetail />
+    </div>
+  );
+}
+
+export default PlayerPage;
