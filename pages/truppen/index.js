b66115b4c3ab82f8d840e6d3613def8cd55254c1
Philip Carlsson <philipcarlsson@hotmail.com>
2022-08-26 18:18:12 +0200

    creating nested route for squad for creating new player

--- pages/truppen/index.js
+++ pages/truppen/index.js
@@ -1,7 +1,22 @@
 import React from "react";
+import Link from "next/link";
 
 function SquadPage() {
-  return <div>index</div>;
+  return (
+    <React.Fragment>
+      <h5>Spelartrupp</h5>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/truppen/ny-spelare">Registera spelare</Link>
+          </li>
+          <li>
+            <Link href="/truppen/hej">as</Link>
+          </li>
+        </ul>
+      </nav>
+    </React.Fragment>
+  );
 }
 
 export default SquadPage;
---
+++ pages/truppen/ny-spelare/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function RegisterPlayer() {
+  return <div>RegisterPlayer</div>;
+}
+
+export default RegisterPlayer;
