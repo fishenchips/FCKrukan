6ea105e18940a03fd6e2431b53ce13e838a0f746
Philip Carlsson <philipcarlsson@hotmail.com>
2022-08-27 10:52:42 +0200

    adding function to create player

--- components/players/NewPlayerForm.js
+++ components/players/NewPlayerForm.js
@@ -9,23 +9,55 @@ function NewPlayerForm() {
     years.push(i);
   }
 
+  const firstNameInputRef = useRef();
+  const lastNameInputRef = useRef();
+  const imageInputRef = useRef();
+  const yearInputRef = useRef();
+  const positionInputRef = useRef();
+  const footInputRef = useRef();
+  const parentClubInputRef = useRef();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const enteredFirstName = firstNameInputRef.current.value;
+    const enteredLastName = lastNameInputRef.current.value;
+    const enteredImage = imageInputRef.current.value;
+    const enteredYear = yearInputRef.current.value;
+    const enteredPosition = positionInputRef.current.value;
+    const enteredFoot = footInputRef.current.value;
+    const enteredParentClub = parentClubInputRef.current.value;
+
+    const playerData = {
+      firstName: enteredFirstName,
+      lastName: enteredLastName,
+      image: enteredImage,
+      year: enteredYear,
+      position: enteredPosition,
+      foot: enteredFoot,
+      parentClub: enteredParentClub,
+    };
+
+    console.log(playerData);
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div>
         <label>Förnamn</label>
-        <input type="text" />
+        <input type="text" ref={firstNameInputRef} />
       </div>
       <div>
         <label>Efternamn</label>
-        <input type="text" />
+        <input type="text" ref={lastNameInputRef} />
       </div>
       <div>
         <label>Bild</label>
-        <input type="file" />
+        <input type="file" ref={imageInputRef} />
       </div>
       <div>
         <label>Födelseår</label>
-        <select>
+        <select ref={yearInputRef}>
           {years.map((year) => (
             <option key={year} value={year}>
               {year}
@@ -36,7 +68,7 @@ function NewPlayerForm() {
       <div>
         <label>Position</label>
         {/* needs more precision, and able to choose serveral positions */}
-        <select>
+        <select ref={positionInputRef}>
           <option value="goal-keeper">Målvakt</option>
           <option value="defender">Back</option>
           <option value="midfielder">Mittfält</option>
@@ -45,7 +77,7 @@ function NewPlayerForm() {
       </div>
       <div>
         <label>Fot</label>
-        <select>
+        <select ref={footInputRef}>
           <option value="right">Höger</option>
           <option value="left">Vänster</option>
           <option value="both">Dubbelfotad</option>
@@ -53,7 +85,7 @@ function NewPlayerForm() {
       </div>
       <div>
         <label>Moderklubb</label>
-        <input type="text" />
+        <input type="text" ref={parentClubInputRef} />
       </div>
       <div>
         <button>Lägg till Spelare</button>
