df66ddedab023bce7cb21f9c07bfac4e82f375d9
Philip Carlsson <philipcarlsson@hotmail.com>
2022-09-15 21:36:06 +0200

    creating todo for next time

--- components/players/PlayerDetail.js
+++ components/players/PlayerDetail.js
@@ -1,7 +1,20 @@
 import React from "react";
 
+/* TODO - read about images and best practice in DB 
+    Create a Link around below to the individual player page where rest of info will be shown
+*/
+
 function PlayerDetail(props) {
-  return <div>PlayerDetail</div>;
+  return (
+    <li>
+      <div>
+        <h4>
+          {props.firstName} {props.lastName}
+        </h4>
+        <img src={props.image} />
+      </div>
+    </li>
+  );
 }
 
 export default PlayerDetail;
--- components/players/PlayerList.js
+++ components/players/PlayerList.js
@@ -4,19 +4,21 @@ import PlayerDetail from "./PlayerDetail";
 function PlayerList(props) {
   return (
     <section>
-      {props.playersList.map((player) => (
-        <PlayerDetail
-          key={player.id}
-          id={player.id}
-          firstName={player.firstName}
-          lastName={player.lastName}
-          image={player.image}
-          year={player.year}
-          position={player.position}
-          foot={player.foot}
-          parentClub={player.parentClub}
-        />
-      ))}
+      <ul>
+        {props.playersList.map((player) => (
+          <PlayerDetail
+            key={player.id}
+            id={player.id}
+            firstName={player.firstName}
+            lastName={player.lastName}
+            image={player.image}
+            year={player.year}
+            position={player.position}
+            foot={player.foot}
+            parentClub={player.parentClub}
+          />
+        ))}
+      </ul>
     </section>
   );
 }
