5089b1292a229462a1596b6c931f008dfc3ad674
Philip Carlsson <philipcarlsson@hotmail.com>
2022-09-15 21:27:57 +0200

    sending down all properties of a player to PlayerDetail

---
+++ components/players/PlayerDetail.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function PlayerDetail(props) {
+  return <div>PlayerDetail</div>;
+}
+
+export default PlayerDetail;
--- components/players/PlayerList.js
+++ components/players/PlayerList.js
@@ -1,13 +1,21 @@
 import React from "react";
+import PlayerDetail from "./PlayerDetail";
 
 function PlayerList(props) {
   return (
     <section>
       {props.playersList.map((player) => (
-        <div key={player.id}>
-          <p>{player.lastName}</p>
-          <p>{player.firstName}</p>
-        </div>
+        <PlayerDetail
+          key={player.id}
+          id={player.id}
+          firstName={player.firstName}
+          lastName={player.lastName}
+          image={player.image}
+          year={player.year}
+          position={player.position}
+          foot={player.foot}
+          parentClub={player.parentClub}
+        />
       ))}
     </section>
   );
