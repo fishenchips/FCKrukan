c7a5e906e6de3afc2e41aa1e0d867d61137fe543
Philip Carlsson <philipcarlsson@hotmail.com>
2022-09-15 13:26:51 +0200

    creating api route to mongoDB cluster

--- pages/api/ny-spelare.js
+++ pages/api/ny-spelare.js
@@ -1,1 +1,30 @@
 import { MongoClient } from "mongodb";
+
+/* Creating API route function to send query to my mongoDB cluster */
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    /* connecting to DB application, generic username and password for now - wont run on client side*/
+    const client = await MongoClient.connect(
+      "mongodb+srv://philip:123@firstcluster.zwayv.mongodb.net/?retryWrites=true&w=majority"
+    );
+
+    const db = client.db();
+
+    const playerCollection = db.collection("players");
+
+    /* insert an object with the needed parameters */
+    const result = await playerCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    /* want to send back an result */
+    res.status(201).json({ message: "Player Added." });
+  }
+}
+
+export default handler;
