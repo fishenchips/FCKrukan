95cb725fb3ccf5bc6ef8e47d9671a7d42b925752
Philip Carlsson <philipcarlsson@hotmail.com>
2022-09-22 13:37:41 +0200

    adding getStaticPaths to individual player page

--- pages/truppen/[playerId]/index.js
+++ pages/truppen/[playerId]/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import PlayerDetail from "../../../components/players/PlayerDetail";
+import { MongoClient } from "mongodb";
+import { APIKey } from "../../../keys/clientKey";
 
 function PlayerPage() {
   return (
@@ -7,6 +9,35 @@ function PlayerPage() {
       <PlayerDetail />
     </div>
   );
+}
+
+export async function getStaticPaths() {
+  const client = await MongoClient.connect(APIKey);
+
+  const db = client.db();
+
+  const playerCollection = db.collection("players");
+
+  /* access to all meetups, we want the ID and no other field values */
+  /* first empty object to (first arg is filter criteria), we want to find all field values */
+  /* second {} for what fields should be extracted, standard is all values are used , 
+  but we only want id
+    _id: 1 === only include the id, nothing else
+    need toArray() to convert to JS array of objects*/
+  const players = await playerCollection.find({}, { _id: 1 }).toArray();
+
+  client.close();
+
+  return {
+    /* any path that isnt included below till show 404 (if false) */
+    fallback: "blocking",
+    /* generating dynamic paths */
+    paths: players.map((player) => ({
+      params: {
+        playerId: player._id.toString(),
+      },
+    })),
+  };
 }
 
 export default PlayerPage;
